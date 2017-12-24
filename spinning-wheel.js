3500b92031803509ab699ec68b8db9bb98bbe27b
Hanse <hkri@koren.im>
2017-12-24 13:54:38 +0100

    add a tiny user interface

--- spinning-wheel.js
+++ spinning-wheel.js
@@ -1,115 +1,170 @@
-const canvas = document.querySelector('canvas');
-const winner = document.querySelector('#winner');
-canvas.width = window.innerHeight - 50;
-canvas.height = window.innerHeight - 50;
-const context = canvas.getContext('2d');
+const PADDING = 15;
 
-const values = [
-  '01 DEN ENKLE',
-  '02 KVESS',
-  '03 DRØMMEN',
-  '04 PIZZABAKEREN SPESIAL',
-  '05 SNADDER',
-  '06 MIX',
-  '07 MEKSIKANEREN',
-  '08 BIFFEN',
-  '09 DEN MARINERTE',
-  '10 PEPPERSVENNEN',
-  '11 FLAMMEN',
-  '12 TACOKYLLINGEN',
-  '13 KOKKENS KYLLING',
-  '14 KOKKENS FAVORITT',
-  '15 GNISTEN',
-  '16 LUKSUSKYLLING',
-  '17 KYLLINGFARMEN',
-  '18 VEGETARIANEREN',
-  '19 KEBABEN',
-  '20 DRENGEN',
-  '21 MR.X',
-  '22 CHORIZO SPESIAL',
-  '23 DOBBELDEKKER',
-  '24 HEIT KYLLING',
-  '25 CHORIZOEN',
-  '26 TOSPANNET',
-  '27 SQUASHEN',
-  '28 HOTTENTOTTEN'
-];
+const defaultValues = `
+01 DEN ENKLE
+02 KVESS
+03 DRØMMEN
+04 PIZZABAKEREN SPESIAL
+05 SNADDER
+06 MIX
+07 MEKSIKANEREN
+08 BIFFEN
+09 DEN MARINERTE
+10 PEPPERSVENNEN
+11 FLAMMEN
+12 TACOKYLLINGEN
+13 KOKKENS KYLLING
+14 KOKKENS FAVORITT
+15 GNISTEN
+16 LUKSUSKYLLING
+17 KYLLINGFARMEN
+18 VEGETARIANEREN
+19 KEBABEN
+20 DRENGEN
+21 MR.X
+22 CHORIZO SPESIAL
+23 DOBBELDEKKER
+24 HEIT KYLLING
+25 CHORIZOEN
+26 TOSPANNET
+27 SQUASHEN
+28 HOTTENTOTTEN
+`.trim();
 
-const colors = ['#771816', '#FFE79F', '#FFD57F', '#F7AC00']
+function shuffle(input) {
+  const array = input.slice();
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
 
-const draw = rotate => {
-  context.clearRect(0, 0, canvas.width, canvas.height);
+const parseParticipants = participants =>
+  participants
+    .split('\n')
+    .filter(Boolean)
+    .map(participant => {
+      if (/(\w+) (\d+)/.test(participant)) {
+        return participant.trim().split(' ');
+      }
 
-  context.translate(canvas.width / 2, canvas.height / 2);
-  context.rotate(rotate);
-  context.translate(-canvas.width / 2, -canvas.height / 2);
+      return [participant, 1];
+    })
+    .map(([name, count]) => Array(parseInt(count, 10)).fill(name))
+    .reduce((acc, item) => [...acc, ...item], []);
 
-  context.fillStyle = '#db9704';
-  context.arc(canvas.width / 2, canvas.height / 2, canvas.height / 2, 0, 2 * Math.PI);
-  context.fill();
+(async () => {
+  const canvas = document.querySelector('canvas');
+  const winner = document.querySelector('#winner');
+  canvas.width = window.innerHeight - 50;
+  canvas.height = window.innerHeight - 50;
 
-  values.forEach((value, i) => {
-    const radian = 2 * Math.PI / values.length;
-    context.fillStyle = `hsl(${i / values.length * 360}, 83%, 60%)`;
-    context.fillStyle = colors[i % colors.length];
-    context.beginPath();
-    context.moveTo(canvas.width / 2, canvas.height / 2);
-    context.arc(
-      canvas.width / 2,
-      canvas.height / 2,
-      canvas.height / 2 - 10,
-      radian * i,
-      radian * (i + 1),
-      false
-    );
-    context.closePath();
-    context.fill();
+  const context = canvas.getContext('2d');
+  context.imageSmoothingEnabled = true;
 
-    context.save();
-    context.translate(canvas.width / 2, canvas.height / 2);
-    context.rotate((i + 0.5) * radian);
+  const radius = canvas.width / 2;
+  const queryString = new URLSearchParams(new URL(window.location.href).search);
 
-    const dx = Math.floor(canvas.width / 2) - canvas.width / 4;
-
-    context.fillStyle = 'black';
-    context.font = `16px Helvetica`;
-    context.fillText(value, dx, 0);
-    context.restore();
-  });
-
-
-};
-
-let startAngle = Math.random() * Math.PI;
-let current = startAngle;
-
-const run = (speed, duration) => {
-  let start = performance.now();
-  let angle = startAngle;
-
-  const render = (now) => {
-    let t = (now - start) / (1000 * duration);
-
-    t = Math.min(1, t);
-    angle = speed * (1 - t);
-
-    current = (current + angle) % (Math.PI * 2);
-
-    draw(angle);
-
-    if (t < 1) {
-      requestAnimationFrame(render);
-    } else {
-      const selected = (values.length - Math.floor(current / ( 2 * Math.PI / values.length)));
-      winner.innerText = values[selected - 1];
-    }
+  const options = {
+    participants: queryString.get('values') || defaultValues,
+    duration: parseInt(queryString.get('duration'), 10) || 10
   };
 
-  requestAnimationFrame(render);
-};
+  document.querySelector('#values').value = options.participants;
+  document.querySelector('#duration').value = options.duration;
 
-canvas.addEventListener('click', () => {
-  run(Math.min(0.4, Math.floor(Math.random() * 10) / 10), 5);
-});
+  const values = shuffle(parseParticipants(options.participants));
 
-draw(startAngle);
+  const colors = ['#771816', '#FFE79F', '#FFD57F', '#F7AC00'];
+
+  const draw = rotate => {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+
+    context.translate(canvas.width / 2, canvas.height / 2);
+    context.rotate(rotate);
+    context.translate(-canvas.width / 2, -canvas.height / 2);
+
+    context.fillStyle = 'rgba(0, 0, 0, 0.1)';
+    context.arc(radius, radius, radius, 0, 2 * Math.PI);
+    context.fill();
+
+    values.forEach((value, i) => {
+      const radian = 2 * Math.PI / values.length;
+      context.fillStyle = colors[i % colors.length];
+      context.beginPath();
+      context.moveTo(radius, radius);
+      context.arc(
+        radius,
+        radius,
+        radius - PADDING,
+        radian * i,
+        radian * (i + 1),
+        false
+      );
+      context.closePath();
+      context.fill();
+
+      context.save();
+      context.translate(radius, radius);
+      context.rotate((i + 0.5) * radian);
+
+      const dx = radius - 2 * PADDING;
+
+      context.fillStyle = 'black';
+      context.textAlign = 'right';
+      context.font = `16px system-ui`;
+      context.fillText(value, dx, 5);
+      context.restore();
+    });
+
+    context.fillStyle = '#FFE79F';
+    context.beginPath();
+    context.arc(radius, radius, radius / 8, 0, 2 * Math.PI);
+    context.fill();
+    context.closePath();
+  };
+
+  let startAngle = Math.random() * Math.PI;
+  let current = startAngle;
+
+  let running = false;
+
+  const run = (speed, duration) => {
+    running = true;
+    let start = performance.now();
+    let angle = startAngle;
+
+    const render = now => {
+      let t = (now - start) / (1000 * duration);
+
+      t = Math.min(1, t);
+      angle = speed * (1 - t);
+
+      current = (current + angle) % (Math.PI * 2);
+
+      draw(angle);
+      if (t < 1) {
+        requestAnimationFrame(render);
+      } else {
+        const selected =
+          values.length - Math.floor(current / (2 * Math.PI / values.length));
+        winner.innerText = values[selected - 1];
+        running = false;
+      }
+    };
+
+    requestAnimationFrame(render);
+  };
+
+  canvas.addEventListener('click', () => {
+    if (values.length === 0) {
+      alert('No tickets available.');
+      return;
+    }
+    if (running) return;
+    run(Math.min(0.4, Math.floor(Math.random() * 10) / 10), options.duration);
+  });
+
+  draw(startAngle);
+})();
